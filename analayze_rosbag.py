29a75b5dbf0e0655e6963267fba9e019f0204204
“cltl1t405” <gorenlaptop1t405@gmail.com>
2017-08-31 13:01:52 +0300

    get poses from data - affdex bug fix

--- analayze_rosbag.py
+++ analayze_rosbag.py
@@ -14,12 +14,16 @@ from os.path import isfile, join
 import numpy as np
 
 count=0
+
 sections = ['learn', 'task1', 'task2', 'task3']
+
+affdex_list=['emotions','expressions','measurements','face_points','face_x_alignment',
+             'face_y_alignment','face_tracking_deltax','face_distance','face_detected','tega_affect_msg','attention_detected']
 
 mypath = '/home/matan/Desktop/data/'
 
 files = [f for f in listdir(mypath) if isfile(join(mypath, f)) and '.bag' in f]
-
+subjects_with_no_angles_topic=[]
 data = {}
 # for each bag file
 for f in files:
@@ -31,6 +35,8 @@ for f in files:
         count+=1
         print "count= ",count
         data[subject_id] = {}
+        data[subject_id][0] = {}
+
 
         # open the bag file
         bag = rosbag.Bag(mypath + f)
@@ -47,109 +53,149 @@ for f in files:
         #affdex:
         affdex=0
 
+
+        #is_nao_angles_topic
+        is_nao_angles_topic=False
+
+        is_working= False
+
         current_skeleton_angle = None
         current_nao_movements = None
 
         first_time = True
-        # try:
 
-        for topic, msg, t in bag.read_messages():
-            # get the first time, for reference
-            if first_time:
-                t0 = t
-                first_time = False
+        try:
 
-            if 'log' in topic:
-                if'current_state' in msg.data:
-                    step=msg.data.split(':')[1]
-                    data[subject_id][step]={}
+            for topic, msg, t in bag.read_messages():
+                # get the first time, for reference
+                if first_time:
+                    t0 = t
+                    first_time = False
 
-                if 'matrix' in msg.data:
-                    matrix=msg.data.split(':')[1]
-                    data[subject_id][step]['matrix'] = np.array(matrix)
+                if 'log' in topic:
+                    if'current_state' in msg.data:
+                        step=int(msg.data.split(': ')[1])
+                        if step in data[subject_id]:
+                            continue
 
-                if'task' in msg.data:
-                    task_full_name=msg.data.split(',')[2]
-                    task=task_full_name.split('/')[-1]
+                        data[subject_id][step]={}
+                        if step == 1:
+                            section_id = 1
+                            print 'here'
+                        else:
+                            section_id = 0
 
+                    if 'matrix' in msg.data:
+                        if step in data[subject_id]:
+                            matrix=msg.data.split(':')[1]
+                            data[subject_id][step]['matrix'] = np.array(matrix)
 
-            if 'flow' in topic:
-                #   get the transformation
-
-                if msg.data.isdigit():
-                    data[subject_id][step]['transformation'] = int(msg.data)
-
-                  # parse the time to sections (learning, task1, task2, task3)
-                if 'start' in msg.data:
-                    print subject_id,step,sections[section_id]
-                    print task,t
-                    data[subject_id][step][sections[section_id]] = {
-                        'start': t,
-                        'stop': None,
-                        'data': [],
-                        'task':task
-                    }
-
-                if 'stop' in msg.data:
-                    data[subject_id][step][sections[section_id]]['stop'] = t
-                    task = 0
-                    section_id += 1
-                    print 'here'
-
-                    if step == 12:
-                        bag.close()
-                        break
-
-                if section_id >= len(sections):
-                    section_id=0
+                    if'task' in msg.data:
+                        task_full_name=msg.data.split(',')[2]
+                        task=task_full_name.split('/')[-1]
 
 
-            # if 'affdex' in topic:
-            #     affdex= msg
+                if 'flow' in topic:
+                    #   get the transformation
+
+                    if msg.data.isdigit():
+                        if step in data[subject_id]:
+                            data[subject_id][step]['transformation'] = int(msg.data)
+
+                      # parse the time to sections (learning, task1, task2, task3)
+                    if 'start' in msg.data:
+                        if step in data[subject_id]:
+                            # print subject_id,step,sections[section_id]
+                            # print task,t
+                            is_working=True
+                            data[subject_id][step][sections[section_id]] = {
+                                'start': t,
+                                'stop': None,
+                                'data': [],
+                                'task':task
+                            }
+
+                    if 'stop' in msg.data:
+                        if step in data[subject_id]:
+                            if sections[section_id] in data[subject_id][step]:
+                                is_working=False
+                                data[subject_id][step][sections[section_id]]['stop'] = t
+                                task = 0
+                                section_id += 1
+
+                                if step == 12:
+                                    bag.close()
+                                    break
+
+                    if section_id >= len(sections):
+                        section_id=0
 
 
-              # for each section
-            # if 'nao_movements' in topic:
-#                #          get command to robot
-                # current_nao_movements = msg.data
+                if 'affdex' in topic:
+                    dict = {}
+                    for m in affdex_list:
+                        dict[m] = eval('msg.' + m)
+                        if m == 'face_points':
+                            n_list = []
+                            o_list = dict[m]
+                            for point in o_list:
+                                n_list.append({"x": point.x, "y": point.y})
+                            dict[m] = n_list
+                    affdex = dict
 
 
-            # if 'skeleton_angle' in topic:
-            #     #       get raw skeleton markers
-            #     current_skeleton_angle = msg.data
-
-            #
-            # if 'nao_angles_topic' in topic:
-            #     #       get command to robot
-            #     list_of_movements_str=msg.data
-            #     list_of_movements=list_of_movements_str[1:-1].split(', ')
-            #
-            #
-            #     current_nao_movements = (list_of_movements[2:4]+[0.0,0.0]+list_of_movements[-6:-4]+[0.0,0.0])
-            #
-            #     current_nao_movements= str([float(x) for x in current_nao_movements])
-            #     current_nao_movements= current_nao_movements[1:-1]
+                  # for each section
+                # if 'nao_movements' in topic:
+                #    #          get command to robot
+                #     current_nao_movements = msg.data
 
 
-                #       dict[id] = dict[section] = array(dict{skeleton, robot, time})
-                # if sections[section_id] in data[subject_id][step]:
-                #     if current_skeleton_angle is not None and current_nao_movements is not None:
-                #         new_data = {
-                #             'time': (t - t0).to_sec(),
-                #             'skeleton': current_skeleton_angle,
-                #             'robot': current_nao_movements,
-                #             'affdex':affdex
-                #         }
-                #         data[subject_id][step][sections[section_id]]['data'].append(new_data)
+                # time between skeleton_angles is +- 0.04
+                # time between robot angles is +- 0.09
 
-        # except:
-        #     print('error')
-        #     data.pop(subject_id)
+                if 'nao_angles_topic' in topic:
+                    #       get command to robot
+                    is_nao_angles_topic = True
+
+                    list_of_movements_str = msg.data
+                    list_of_movements = list_of_movements_str[1:-1].split(', ')
+
+                if 'skeleton_angle' in topic:
+                    #       get raw skeleton markers
+                    current_skeleton_angle = msg.data
+
+
+
+                    current_nao_movements = (list_of_movements[2:4]+[0.0,0.0]+list_of_movements[-6:-4]+[0.0,0.0])
+
+                    current_nao_movements= str([float(x) for x in current_nao_movements])
+                    current_nao_movements= current_nao_movements[1:-1]
+
+
+                        #dict[id] = dict[section] = array(dict{skeleton, robot, time})
+                    if is_working==True:
+                        if step in data[subject_id]:
+                            if sections[section_id] in data[subject_id][step]:
+                                if current_skeleton_angle is not None and current_nao_movements is not None:
+                                    new_data = {
+                                        'time': (t - t0).to_sec(),
+                                        'skeleton': current_skeleton_angle,
+                                        'robot': current_nao_movements,
+                                        'affdex':affdex
+                                    }
+                                    data[subject_id][step][sections[section_id]]['data'].append(new_data)
+
+            if is_nao_angles_topic==False:
+                subjects_with_no_angles_topic.append(subject_id)
+
+        except:
+            print('error')
+            data.pop(subject_id)
 
 
 print(data.keys())
+print 'subjects_with_no_angles_topic: ',subjects_with_no_angles_topic
+
 pickle.dump(obj=data, file=open('raw_data_all', 'wb'))
 
 
-#todo: bugs - 43
-#todo: time to update data - when bitwin start and srop..
---
+++ get_poses_from_raw_data.py
@@ -0,0 +1,114 @@
+
+###Imports:
+import pickle
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from scipy.signal import medfilt
+import seaborn as sns
+import scipy.optimize
+from numpy.linalg import inv
+from sklearn.cluster import KMeans
+import statsmodels.formula.api as sm
+import seaborn as sns
+from mpl_toolkits.mplot3d import Axes3D
+
+###Parameters:
+median_filter_window = 31
+movement_threshold = 0.002
+
+####subject 51 has no_angles_topic
+
+### Get poses from data:
+def get_poses(angles):
+    t = angles[:, 0]
+    # plt.plot(t, angles[:,1:])
+
+    f_angles = medfilt(angles[:, 1:], [median_filter_window, 1])
+    # plt.plot(t, f_angles)
+
+    d_angles = np.gradient(f_angles, axis=0)
+    total_derivative = np.sum(d_angles ** 2, axis=1)
+    # plt.plot(t, total_derivative)
+    # plt.show()
+
+    # binarize to movement/no-movement
+    bin = np.argwhere(total_derivative < movement_threshold)
+    bin = np.array([x[0] for x in bin])
+    # print(bin)
+    d_bin = bin[1:] - bin[:-1]
+    # print(d_bin)
+
+    # get start/stop of no movement
+    start_stop = np.argwhere(d_bin > 1)
+    no_movement_bins_start = np.array([x[0] for x in bin[start_stop[:-1]+1]])
+    no_movement_bins_stop = np.array([x[0] for x in bin[start_stop[1:]]])
+    # print(no_movement_bins_start, no_movement_bins_stop)
+    # get angles of middle of no-movement section
+    middle_bin = [(no_movement_bins_start[i] + no_movement_bins_stop[i])/2.0 for i in range(no_movement_bins_start.shape[0])]
+    middle_bin = [int(x) for x in middle_bin]
+    # print(middle_bin)
+
+    pose = angles[middle_bin,:]
+    return pose ,middle_bin
+
+#Save skeleton robot poses, depending on delay:
+for delay in range(0,50):
+    print delay
+    data = pickle.load(open('data_test_1', 'rb'))
+    # data[id][step][section] = array(dict{skeleton, robot, time})
+    poses = {}
+
+    for subject_id, step in data.items():           # go over subject
+        print subject_id
+        poses[subject_id] = {}
+        for step_id, step in step.items():    # go over steps
+            for section_id in ['learn', 'task1', 'task2', 'task3']:
+                if section_id in data[subject_id][step_id].keys():
+                    print section_id
+                    section=data[subject_id][step_id][section_id]
+
+                    time_stamp = np.zeros([len(section['data']), 1])
+
+                    skeleton_angles = np.zeros([len(section['data']), 8])
+
+                    robot_angles=np.zeros([len(section['data']), 8])
+
+                    affdex_data=[]
+
+                    for i, d in enumerate(section['data']): # go over time-steps
+
+                        time_stamp[i,0] = d['time']
+
+                        skeleton_angles[i, :] = np.array([float(x) for x in d['skeleton'].split(',')])
+
+                        robot_angles[i, :] = np.array([float(x) for x in d['robot'].split(',')])
+
+                        affdex_data.append(d['affdex'])
+
+                    skeleton_poses, pose_bins = get_poses(skeleton_angles)
+
+
+                   # skeleton_poses=skeleton_poses[:-3]
+                   # pose_bins=pose_bins[:-3]
+
+                robot_poses = robot_angles[[x+delay for x in pose_bins],:]
+
+                time_stamp=time_stamp[pose_bins,:]
+
+                affdex = []
+
+                for i in pose_bins:
+                    affdex.append(affdex_data[i])
+
+                poses[subject_id][section_id] = {
+                    'time': time_stamp,
+                    'skeleton': skeleton_poses,
+                    'robot': robot_poses,
+                    'affdex':affdex
+                }
+
+    pickle.dump(obj=poses, file=open('../physical_curiosity_analysis/data_after_analysis_'+str(delay), 'wb'))
+
+
+#todo - bug fix !
